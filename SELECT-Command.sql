252268685916fd2b5032e303d0470fad89f41432
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-08-10 11:54:10 +0530

    SELECT command to view columns(data)

---
+++ SELECT-Command.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE animal;
+USE animal;
+
+CREATE TABLE herbi (
+  id INT PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+-- to select & view all columns
+SELECT * FROM herbi;
+
+-- or
+
+-- to select DATABASE
+USE hospital; 
+
+SELECT * FROM patient;
